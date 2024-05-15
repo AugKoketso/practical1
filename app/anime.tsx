84e26ffff52eede52a330474e9ae8f9815762faf
AugustMk <augustinekoketso4@gmail.com>
2024-05-15 19:46:33 +0200

    added more epiosdes , text too small

--- app/anime.tsx
+++ app/anime.tsx
@@ -1,9 +1,59 @@
-"use client";
+"use client"; 
 import React, { useEffect, useState, CSSProperties } from 'react';
 
 const BoxComponent = () => {
-  // State to handle the text dynamically
-  const [crawlText, setCrawlText] = useState("Episode IV, A NEW HOPE\n\nIt is a period of civil war.\nRebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire..");
+  // State to handle the text dynamically, now including more content and formatted for multiline
+  const [crawlText, setCrawlText] = useState(`Episode IV, 
+  A NEW HOPE
+   It is a period of civil war.
+  Rebel spaceships, striking
+  from a hidden base, have won
+  their first victory against
+  the evil Galactic Empire.
+  
+  During the battle, Rebel
+  spies managed to steal secret
+  plans to the Empire's
+  ultimate weapon, the DEATH
+  STAR, an armored space
+  station with enough power to
+  destroy an entire planet.
+  
+  Pursued by the Empire's
+  sinister agents, Princess
+  Leia races home aboard her
+  starship, custodian of the
+  stolen plans that can save
+  her people and restore
+  freedom to the galaxy....
+  
+  
+
+
+  
+Episode V
+THE EMPIRE STRIKES BACK
+It is a dark time for the
+Rebellion. Although the Death
+Star has been destroyed,
+Imperial troops have driven the
+Rebel forces from their hidden
+base and pursued them across
+the galaxy.
+
+Evading the dreaded Imperial
+Starfleet, a group of freedom
+fighters led by Luke Skywalker
+has established a new secret
+base on the remote ice world
+of Hoth.
+
+The evil lord Darth Vader,
+obsessed with finding young
+Skywalker, has dispatched
+thousands of remote probes into
+the far reaches of space....
+  `);
 
   // Inline styles for the starfield box
   const boxStyle: CSSProperties = {
