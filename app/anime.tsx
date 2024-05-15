b1146daa9cb463e9e9d281cd444d79918dc87d8a
AugustMk <augustinekoketso4@gmail.com>
2024-05-15 20:41:07 +0200

    STYLED THE TEXT

--- app/anime.tsx
+++ app/anime.tsx
@@ -53,7 +53,40 @@ obsessed with finding young
 Skywalker, has dispatched
 thousands of remote probes into
 the far reaches of space....
-  `);
+ 
+
+
+
+Episode VI
+RETURN OF THE JEDI
+Luke Skywalker has returned to
+his home planet of Tatooine in
+an attempt to rescue his
+friend Han Solo from the
+clutches of the vile gangster
+Jabba the Hutt.
+
+Little does Luke know that the
+GALACTIC EMPIRE has secretly
+begun construction on a new
+armored space station even
+more powerful than the first
+dreaded Death Star.
+
+When completed, this ultimate
+weapon will spell certain doom
+for the small band of rebels
+struggling to restore freedom
+to the galaxy...
+
+
+
+DOU REALLY WANT THE REST???
+
+
+
+
+`);
 
   // Inline styles for the starfield box
   const boxStyle: CSSProperties = {
